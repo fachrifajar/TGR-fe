fe282ca2e18952d10abda5337b62b5e188d09764
Fachri Fajar <101514724+fachrifajar@users.noreply.github.com>
2023-11-24 20:24:18 +0700

    add loremipsum2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
     <>
       <Appbar />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit consectetur id accusamus architecto, unde natus! Architecto doloremque neque repudiandae ducimus doloribus, deleniti tempore iusto tenetur, nobis nulla placeat voluptate consectetur!
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum odit facere doloremque eveniet aliquam, odio laboriosam architecto vel quo veritatis neque sequi voluptatum illum, deleniti cumque porro. Debitis, magnam inventore.
     </>
   );
 };
