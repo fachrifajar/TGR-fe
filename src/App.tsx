516c425c137f1a80f86b5013ce3b5f285c4e7c93
Fachri Fajar <101514724+fachrifajar@users.noreply.github.com>
2023-11-24 21:26:43 +0700

    feature 4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
       test
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis perspiciatis impedit ratione saepe delectus voluptatibus. Consectetur illo iure, nam itaque expedita veniam veritatis doloribus, autem ipsam, cum suscipit quia.
 
-      feature 1
+      feature 4
       feature 2
       feature 3
     </>
