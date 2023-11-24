e6cd8496c1bc5c1597b7248131fd57d294a92b6f
Fachri Fajar <101514724+fachrifajar@users.noreply.github.com>
2023-11-24 21:07:47 +0700

    -

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ const App = () => {
   return (
     <>
       <Appbar />
+      test
+      test
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis perspiciatis impedit ratione saepe delectus voluptatibus. Consectetur illo iure, nam itaque expedita veniam veritatis doloribus, autem ipsam, cum suscipit quia.
     </>
   );
 };
